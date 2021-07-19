07c1d50bb9f929ee33940cccf6e172e08c4aa436
hassa <hassangames1998@gmail.com>
2021-07-19 21:37:38 +0300

    Testing Release

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy as db
 
 Base = declarative_base()
-engine = db.create_engine("sqlite:///db.sqlite")
+engine = db.create_engine("sqlite:///telegram.db")
 
 session = sessionmaker()
 session.configure(bind=engine)
@@ -11,7 +11,7 @@ my_session = session()
 
 
 class Users(Base):
-    __tablename__ = "temp_users"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     telegram_id = db.Column(db.Integer)
     name = db.Column(db.String())
@@ -26,36 +26,84 @@ class Users(Base):
         return True if self.is_active else False
 
     def __repr__(self):
-        return self.id
+        return str(self.id)
 
 
 class MasersAPI(Base):
-    __tablename__ = "temp_masersapi"
+    __tablename__ = "masterapi"
     id = db.Column(db.Integer, primary_key=True)
-    owner = db.ForeignKey('Users.id')
+    owner = db.Column(db.Integer)
     api = db.Column(db.String())
-    is_active = db.Column(db.Boolean, default=True)
+    is_active = db.Column(db.Integer, default=True)
 
     def GetActiveMasters(self):
         return 'query all enabled masters'
 
     def __repr__(self):
-        return self.id
+        return str(self.id)
 
 
 class Slaves(Base):
-    __tablename__ = "temp_slaves"
+    __tablename__ = "slaves"
     id = db.Column(db.Integer, primary_key=True)
-    parent = db.ForeignKey('MasersAPI.id')
+    parent = db.Column(db.Integer)
     slave = db.Column(db.String())
     is_active = db.Column(db.Boolean, default=True)
 
-    def GetActiveSlaves(self):
-        return 'query all enabled slaves'
-
     def __repr__(self):
-        return self.id
+        return str(self.id)
 
 
-query = dir(session)
-print(query)
+def LoadSlaves(master_id):
+    slaves = my_session.query(Slaves).filter(Slaves.parent == master_id).all()
+    session.close_all()
+    return [slave for slave in slaves]
+
+
+def LoadUserIDByTelegramID(telegram_id):
+    user_id = my_session.query(Users).filter(Users.telegram_id == telegram_id).first()
+    print(user_id)
+    return user_id.id
+
+
+def ChangeMasterStatus(master_id, status):
+    master_key = my_session.query(MasersAPI).filter(MasersAPI.id == master_id).first()
+    master_key.is_active = status
+    my_session.commit()
+    return True
+
+
+def LoadMasterKey(master_id):
+    return my_session.query(MasersAPI).filter(MasersAPI.id == master_id).first().api
+
+
+def DeleteMaster(master_id):
+    master_key = my_session.query(MasersAPI).filter(MasersAPI.id == master_id).first()
+    my_session.delete(master_key)
+    my_session.commit()
+    return True
+
+
+def LoadMasters(user_id):
+    masters = my_session.query(MasersAPI).where(MasersAPI.owner == user_id).all()
+    print(masters)
+    return masters
+
+
+def IsUserAuthorized(user_id):
+    user = my_session.query(Users).filter(Users.id == user_id).first()
+    if user and bool(user.is_active):
+        return True
+    else:
+        return False
+
+
+def StoreMasterKey(**kwargs):
+    api = kwargs.get('api')
+    owner = kwargs.get('owner')
+    my_session.add(MasersAPI(api=api, owner=owner))
+    my_session.commit()
+
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,33 +1,83 @@
 import requests
 
 API = '1903460584:AAFC6O9Gk6jrVWpxC91-Z2TryOWX0L7sPr8'
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, Filters
+import db
 
 allowed_users = [669065474]
 
-masters = ['abcdefee', 'ecdefee', 'webcdefee']
-slaves = {masters[0]: ['123', '456', '789'],
-          }
+
+def handleBoolean(status):
+    return 'Enabled' if status == 1 else 'Disabled'
+
+
+StartUPKeyboard = [
+    ['/start', '/addmaster'],
+    ['/Done'],
+]
+markup = ReplyKeyboardMarkup(StartUPKeyboard,
+                             one_time_keyboard=True,
+                             )
+
+
+def ViewUserMasters(telegram_id):
+    # TODO VIEW ALL USER MASTER KEY AND RETURN THEM IN INTERACTIVE WAY
+    telegramUserID = telegram_id
+    user_id = db.LoadUserIDByTelegramID(telegram_id=telegramUserID)
+    masters = db.LoadMasters(user_id=user_id)
+    keyboard = []
+    for index, master in enumerate(masters):
+        keyboard.append(
+            [InlineKeyboardButton(f"{master.id}", callback_data=f'viewKey-{master.id}'),
+             InlineKeyboardButton("View Slaves", callback_data=f"ViewSlaves-{master.id}"),
+             InlineKeyboardButton(f"{handleBoolean(master.is_active)}",
+                                  callback_data=f'changeStatus-{master.id}-{master.is_active}'),
+             InlineKeyboardButton("Delete", callback_data=f"deletemaster-{master.id}")]
+        )
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    return reply_markup
+
+
+def ViewMasterSlave(update: Update, context: CallbackContext):
+    # TODO VIEW ALL Slaves RELATED TO Master  AND RETURN THEM IN INTERACTIVE WAY
+    pass
 
 
 def start(update: Update, context: CallbackContext):
-
     keyboard = [
-        [InlineKeyboardButton("print my information", callback_data='myinfo')],
-        [InlineKeyboardButton("Am I permitted", callback_data='perm')]
+        [InlineKeyboardButton("View my masters", callback_data='ViewMasters')],
+        [InlineKeyboardButton("Under process", callback_data='perm')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     message_reply_text = 'Chose operation'
     update.message.reply_text(message_reply_text, reply_markup=reply_markup)
 
 
-def myinfo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_document(document=open('main.py', 'rb'))
+def UserInputHandler(update: Update, context: CallbackContext):
+    command = update.message.text
+    user_id = db.LoadUserIDByTelegramID(update.effective_user.id)
+    if db.IsUserAuthorized(user_id):
+        if command.startswith('/addmaster'):
+            if len(command.split("-")) > 1:
+                db.StoreMasterKey(api=command.split("-")[-1], owner=user_id)
+                update.message.reply_text("Master key has been stored successfully")
+            else:
+                update.message.reply_text("Please enter valid command\n /addmaster-abc123")
+        else:
+            update.message.reply_text("Wrong Input")
+    else:
+        update.message.reply_text("UNAUTHORIZED")
 
 
-def Echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("خذ _!_")
+def ViewAllSlaves(update: Update, context: CallbackContext):
+    slaves = []
+    for slave in db.LoadSlaves(1):
+        slaves.append([InlineKeyboardButton(slave.id, callback_data='myinfo'),
+                       InlineKeyboardButton(slave.parent, callback_data='myinfo')])
+    reply_markup = InlineKeyboardMarkup(slaves)
+    update.message.reply_text("Slaves", reply_markup=reply_markup)
 
 
 def getClickButtonData(update, context):
@@ -42,23 +92,45 @@ def getClickButtonData(update, context):
 
     if update.callback_query.data == "myinfo":
         update.callback_query.message.reply_text(f'Hello {update.effective_user.id}, {update.effective_user.full_name}')
+    # Return all master key to user
+    if update.callback_query.data == 'ViewMasters':
+        update.callback_query.message.reply_text("Your Masters are:",
+                                                 reply_markup=ViewUserMasters(update.effective_user.id))
+
+    # delete master key by id
+    if update.callback_query.data.startswith("deletemaster"):
+        master_id = update.callback_query.data.split('-')[-1]
+        db.DeleteMaster(master_id)
+        update.callback_query.message.reply_text("Master deleted {}".format(master_id),
+                                                 reply_markup=ViewUserMasters(update.effective_user.id))
+    # print all slaves by master id
+    if update.callback_query.data.startswith("ViewSlaves"):
+        update.callback_query.message.reply_text(
+            "Viewing Slaves that belongs to: {}".format(update.callback_query.data.split('-')[-1]))
+    # disable or enable master key
+    if update.callback_query.data.startswith("changeStatus"):
+        print(update.callback_query.data)
+        master_id = update.callback_query.data.split('-')[1]
+        status = update.callback_query.data.split('-')[-1]
+        if int(status) != 1:
+            if db.ChangeMasterStatus(master_id, 1):
+                update.callback_query.message.reply_text("Master has status been enabled".title(),
+                                                         reply_markup=ViewUserMasters(update.effective_user.id))
+        else:
+            if db.ChangeMasterStatus(master_id, 0):
+                update.callback_query.message.reply_text("Master has status been disabled".title(),
+                                                         reply_markup=ViewUserMasters(update.effective_user.id))
+    if update.callback_query.data.startswith('viewKey'):
+        master_id = update.callback_query.data.split('-')[-1]
+        api_string = db.LoadMasterKey(master_id)
+        update.callback_query.message.reply_text(api_string)
 
 
-def perm(update: Update, context: CallbackContext) -> None:
-    print(update.effective_user.id)
-    if int(update.effective_user.id) in allowed_users:
-        update.message.reply_text(f'user{update.effective_user.full_name} is allowed to access this command')
-    else:
+updater = Updater(API, use_context=True)
 
-        update.message.reply_text(f'user{update.effective_user.full_name} has no permissions to access this command ')
-
-
-updater = Updater(API)
-
-updater.dispatcher.add_handler(CommandHandler('hello', myinfo))
-updater.dispatcher.add_handler(CommandHandler('perm', perm))
-updater.dispatcher.add_handler(CommandHandler('get', Echo))
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, UserInputHandler))
+
 updater.dispatcher.add_handler(CallbackQueryHandler(getClickButtonData))
 updater.start_polling()
 updater.idle()
